732d7af0b682c7cea37cf511286f13faf0e61bbe
olb26 <olb26@drexel.edu>
2024-10-20 21:31:22 -0400

    app route for rendering database tables page

--- data.py
+++ data.py
@@ -1,4 +1,5 @@
-from flask import Blueprint
+from flask import Blueprint, render_template, request
+import sqlite3 as sql
 
 data_bp = Blueprint('data', __name__)
 
@@ -8,7 +9,24 @@ data_bp = Blueprint('data', __name__)
 
 @data_bp.route('/')
 def data():
-    return 'Main page for what will be the data modifier section!'
+    db_file = request.args.get('db', 'RWComplete.db')
+    with sql.connect(db_file) as conn:
+        cursor = conn.cursor()
+        cursor.execute("SELECT name FROM sqlite_master WHERE type='table';")
+        tables = cursor.fetchall()
+        data = {}
+
+        for table_name in tables:
+            table = table_name[0]
+            cursor.execute(f"SELECT * FROM {table};")
+            rows = cursor.fetchall()
+
+            cursor.execute(f"PRAGMA table_info({table});")
+            columns = [col[1] for col in cursor.fetchall()]
+
+            data[table] = {'columns': columns, 'rows': rows}
+        
+    return render_template('db_view.html', data=data)
 
 # We can also treat this file as a command line script and forget we are using flask.
 def runs_only_when_ran_from_command_line():
