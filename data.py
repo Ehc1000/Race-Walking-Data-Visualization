b34af164f1c84b7d3044ec94e572befa2efd6398
olb26 <olb26@drexel.edu>
2024-11-04 18:38:06 -0500

    feat: Load tables only when scrolled into view in order to improve page load speed

--- data.py
+++ data.py
@@ -3,10 +3,6 @@ import sqlite3 as sql
 
 data_bp = Blueprint("data", __name__)
 
-# The data modification section. Making a note here for those reading this to google the datatables library,
-# to do ninety percent of the work for you.
-# https://datatables.net/
-
 
 @data_bp.route("/")
 def data():
@@ -14,20 +10,23 @@ def data():
     with sql.connect(db_file) as conn:
         cursor = conn.cursor()
         cursor.execute("SELECT name FROM sqlite_master WHERE type='table';")
-        tables = cursor.fetchall()
-        data = {}
+        tables = [table[0] for table in cursor.fetchall()]
+    return render_template("db_view.html", tables=tables)
 
-        for table_name in tables:
-            table = table_name[0]
-            cursor.execute(f"SELECT * FROM {table};")
-            rows = cursor.fetchall()
 
-            cursor.execute(f"PRAGMA table_info({table});")
-            columns = [col[1] for col in cursor.fetchall()]
+@data_bp.route("/load_table/<table>")
+def load_table(table):
+    db_file = request.args.get("db", "RWComplete.db")
+    with sql.connect(db_file) as conn:
+        cursor = conn.cursor()
+        cursor.execute(f"SELECT * FROM {table};")
+        rows = cursor.fetchall()
+        cursor.execute(f"PRAGMA table_info({table});")
+        columns = [col[1] for col in cursor.fetchall()]
 
-            data[table] = {"columns": columns, "rows": rows}
-
-    return render_template("db_view.html", data=data)
+    return render_template(
+        "table_partial.html", table=table, content={"columns": columns, "rows": rows}
+    )
 
 
 @data_bp.route("/update/<table>/<column>/<pk>", methods=["PATCH"])
@@ -43,11 +42,17 @@ def update_cell(table, column, pk):
                 f"UPDATE {table} SET {column} = ? WHERE rowid = ?", (new_value, pk)
             )
             conn.commit()
-            return f"""
+            return create_input_cell_html(request.path, new_value)
+    except Exception as e:
+        return str(e), 400
+
+
+def create_input_cell_html(request_path: str, new_value: str) -> str:
+    return f"""
                 <td>
                     <div class="editable"
                         hx-target="closest td"
-                        hx-patch="{request.path}"
+                        hx-patch="{request_path}"
                         contenteditable="true"
                         hx-trigger="blur"
                         hx-swap="outerHTML"
@@ -57,14 +62,3 @@ def update_cell(table, column, pk):
                     <input type="hidden" name="value" value="{new_value}" />
                 </td>
             """
-    except Exception as e:
-        return str(e), 400
-
-
-# We can also treat this file as a command line script and forget we are using flask.
-def runs_only_when_ran_from_command_line():
-    print("This function runs when the script is executed directly!")
-
-
-if __name__ == "__main__":
-    runs_only_when_ran_from_command_line()
