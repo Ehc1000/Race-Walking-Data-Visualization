686af7c253ba7de31f3317d0e9ba79cdd1d4f46c
olb26 <olb26@drexel.edu>
2024-11-04 18:25:08 -0500

    feat: add a new endpoint for updating db table cells

--- data.py
+++ data.py
@@ -1,15 +1,16 @@
 from flask import Blueprint, render_template, request
 import sqlite3 as sql
 
-data_bp = Blueprint('data', __name__)
+data_bp = Blueprint("data", __name__)
 
 # The data modification section. Making a note here for those reading this to google the datatables library,
 # to do ninety percent of the work for you.
 # https://datatables.net/
 
-@data_bp.route('/')
+
+@data_bp.route("/")
 def data():
-    db_file = request.args.get('db', 'RWComplete.db')
+    db_file = request.args.get("db", "RWComplete.db")
     with sql.connect(db_file) as conn:
         cursor = conn.cursor()
         cursor.execute("SELECT name FROM sqlite_master WHERE type='table';")
@@ -24,13 +25,46 @@ def data():
             cursor.execute(f"PRAGMA table_info({table});")
             columns = [col[1] for col in cursor.fetchall()]
 
-            data[table] = {'columns': columns, 'rows': rows}
-        
-    return render_template('db_view.html', data=data)
+            data[table] = {"columns": columns, "rows": rows}
+
+    return render_template("db_view.html", data=data)
+
+
+@data_bp.route("/update/<table>/<column>/<pk>", methods=["PATCH"])
+def update_cell(table, column, pk):
+    new_value = request.form.get("value")
+    print(f"Form data: {request.form}")
+    db_file = request.args.get("db", "RWComplete.db")
+
+    try:
+        with sql.connect(db_file) as conn:
+            cursor = conn.cursor()
+            cursor.execute(
+                f"UPDATE {table} SET {column} = ? WHERE rowid = ?", (new_value, pk)
+            )
+            conn.commit()
+            return f"""
+                <td>
+                    <div class="editable"
+                        hx-target="closest td"
+                        hx-patch="{request.path}"
+                        contenteditable="true"
+                        hx-trigger="blur"
+                        hx-swap="outerHTML"
+                        oninput="this.parentElement.querySelector('input').value = this.innerText">
+                        {new_value}
+                    </div>
+                    <input type="hidden" name="value" value="{new_value}" />
+                </td>
+            """
+    except Exception as e:
+        return str(e), 400
+
 
 # We can also treat this file as a command line script and forget we are using flask.
 def runs_only_when_ran_from_command_line():
     print("This function runs when the script is executed directly!")
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     runs_only_when_ran_from_command_line()
