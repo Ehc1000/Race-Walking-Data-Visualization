27b54a8214504ce7b6fd348a1026941eedcf4c6e
byostap <ceostap@gmail.com>
2025-02-10 19:15:09 -0500

    feat: allow user to select which column to order table results by (#15)
    Co-authored-by: olb26 <olb26@drexel.edu>

--- data.py
+++ data.py
@@ -6,11 +6,15 @@ data_bp = Blueprint("data", __name__)
 
 @data_bp.route("/")
 def data():
-    db_file = request.args.get('db', 'db/RWComplete.db')
+    db_file = request.args.get("db", "db/RWComplete.db")
     with sql.connect(db_file) as conn:
         cursor = conn.cursor()
         cursor.execute("SELECT name FROM sqlite_master WHERE type='table';")
-        tables = [table[0] for table in cursor.fetchall() if ('Old' not in table[0]) and ('Backup' not in table[0])]
+        tables = [
+            table[0]
+            for table in cursor.fetchall()
+            if ("Old" not in table[0]) and ("Backup" not in table[0])
+        ]
         tables.sort()
     return render_template("db_view.html", tables=tables)
 
@@ -18,18 +22,44 @@ def data():
 @data_bp.route("/load_table/<table>")
 def load_table(table):
     db_file = request.args.get("db", "RWComplete.db")
+
     page = request.args.get("page", 1, type=int)
     per_page = request.args.get("per_page", 10, type=int)
+    sort_by = request.args.get("sort", "")
+    order = request.args.get("order", "asc")
+
     offset = (page - 1) * per_page
+    order_clause = f"ORDER BY {sort_by} {order.upper()}" if sort_by else ""
+
     with sql.connect(db_file) as conn:
         cursor = conn.cursor()
-        cursor.execute(f"SELECT * FROM {table} LIMIT ? OFFSET ?;", (per_page, offset))
+
+        cursor.execute(f"SELECT COUNT(*) FROM {table}")
+        total_rows = cursor.fetchone()[0]
+
+        cursor.execute(
+            f"SELECT * FROM {table} {order_clause} LIMIT ? OFFSET ?;",
+            (per_page, offset),
+        )
         rows = cursor.fetchall()
+
         cursor.execute(f"PRAGMA table_info({table});")
         columns = [col[1] for col in cursor.fetchall()]
+        total_pages = (total_rows + per_page - 1) // per_page
 
     return render_template(
-        "table_partial.html", table=table, content={"columns": columns, "rows": rows}, page=page
+        "table_partial.html",
+        table=table,
+        content={
+            "columns": columns,
+            "rows": rows,
+            "current_page": page,
+            "total_pages": total_pages,
+            "total_rows": total_rows,
+            "sort_by": sort_by,
+            "order": order,
+        },
+        page=page,
     )
 
 
